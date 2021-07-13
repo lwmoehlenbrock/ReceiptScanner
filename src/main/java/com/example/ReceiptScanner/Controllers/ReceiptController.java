53240252de09da0f6e372229d5031bed5264712e
Lucas <lwmoehlenbrock@ucdavis.edu>
2021-07-13 10:11:21 -0500

    Added code for uploading images to imgur

--- src/main/java/com/example/ReceiptScanner/Controllers/ReceiptController.java
+++ src/main/java/com/example/ReceiptScanner/Controllers/ReceiptController.java
@@ -1,11 +1,37 @@
 package com.example.ReceiptScanner.Controllers;
 
+import com.example.ReceiptScanner.Services.ReceiptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @RestController
-@RequestMapping("/receipts")
+//@RequestMapping("/receipts")
 public class ReceiptController {
+
+    Logger log = LoggerFactory.getLogger(ReceiptController.class);
+
+    @GetMapping("/receipts/test")
+    public String debugs(){
+        return "localhost is working";
+    }
+
+    @PostMapping("/receipts/scan")
+    public void scanReceipt(@RequestBody File image) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(ReceiptService.upload(image).getInputStream()));
+        String inputLine;
+        StringBuilder output = new StringBuilder();
+        while((inputLine=in.readLine()) != null){
+            output.append(inputLine).append("\n");
+        }
+        log.info(output.toString());
+    }
 }
--- src/main/java/com/example/ReceiptScanner/Services/ReceiptService.java
+++ src/main/java/com/example/ReceiptScanner/Services/ReceiptService.java
@@ -2,6 +2,34 @@ package com.example.ReceiptScanner.Services;
 
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 @Service
 public class ReceiptService {
+    public static HttpURLConnection upload(File image) throws IOException {
+        URL path = new URL("https://api.imgur/3/image");
+        HttpURLConnection conn = (HttpURLConnection) path.openConnection();
+
+        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+        byte[] b = new byte[(int) image.length()];
+        FileInputStream fs = new FileInputStream(image);
+        fs.read(b);
+        fs.close();
+        String base64 = URLEncoder.encode(DatatypeConverter.printBase64Binary(b),"UTF-8");
+
+        writer.write("image=" + base64);
+        writer.flush();
+        writer.close();
+
+        return conn;
+
+    }
 }
