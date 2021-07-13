1a16c77336bd08997952e01ca1ad1c193477033a
alejandrocanseco <alejandrocanseco2023@gmail.com>
2021-07-13 16:32:49 -0400

    fixed user controller

--- src/main/java/com/example/ReceiptScanner/Loading/LoadingUsers.java
+++ src/main/java/com/example/ReceiptScanner/Loading/LoadingUsers.java
@@ -1,25 +1,30 @@
-//package com.example.ReceiptScanner.Loading;
-//
-//import com.example.ReceiptScanner.Model.Receipt;
-//import com.example.ReceiptScanner.Model.User;
-//import com.example.ReceiptScanner.Repositories.UserRepository;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class LoadingUsers {
-//    private static final Logger log = LoggerFactory.getLogger(LoadingUsers.class);
-//    List<User> UserList = new ArrayList<>();
-//    List<Receipt> receiptList = new ArrayList<>();
-//    List<Receipt> receiptList = new ArrayList<>();
-//
-//    @Bean
-//    CommandLineRunner initCustomerDB(UserRepository repository) {
-//        return args -> {
-//            log.info("Preloading " + repository.save(new User("Juan taco", 200,300,
-//    }
-//}
+package com.example.ReceiptScanner.Loading;
+
+import com.example.ReceiptScanner.Model.Account;
+import com.example.ReceiptScanner.Model.Receipt;
+import com.example.ReceiptScanner.Model.User;
+import com.example.ReceiptScanner.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class LoadingUsers {
+    private static final Logger log = LoggerFactory.getLogger(LoadingUsers.class);
+    List<User> UserList = new ArrayList<>();
+    List<Receipt> receiptList = new ArrayList<>();
+    List<Account> accountsList = new ArrayList<>();
+
+
+    @Bean
+    CommandLineRunner initCustomerDB(UserRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new User("Juan taco", 200,300,accountsList,receiptList)));
+    };
+}
+}
